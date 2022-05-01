95aa4910489a8aef234fdf418f03c69eae126cc0
Samat Aristanov <Rastir1995@yandex.ru>
2022-05-01 14:18:25 +0400

    доабавил profile и страницу поста

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
-from .models import Group, Post
+from .models import Group, Post, User
 
 
 def index(request):
@@ -29,5 +29,33 @@ def group_posts(request, slug):
         'group': group,
         'posts': posts,
         'button': False
+    }
+    return render(request, template, context=context)
+
+
+def profile(request, username):
+    template = 'posts/profile.html'
+    user = get_object_or_404(User, username=username)
+    post_list = Post.objects.filter(author__username=username)
+    posts_count = post_list.count()
+    paginator = Paginator(post_list, settings.POSTS_FOR_ONE_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'user': user,
+        'page_obj': page_obj,
+        'button': True,
+        'posts_count': posts_count
+    }
+    return render(request, template, context=context)
+
+
+def post_detail(request, post_id):
+    template = 'posts/post_detail.html'
+    post = get_object_or_404(Post, pk=post_id)
+    posts_count = Post.objects.filter(author=post.author).count()
+    context = {
+        'post': post,
+        'posts_count': posts_count,
     }
     return render(request, template, context=context)
